e319c5da11ee73c57cef801a31c3ec13f0ed4fc2
ataberkakkan <akkanataberk@gmail.com>
2024-10-28 16:35:09 +0300

    Reusable Auth Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm.";
+import { signInSchema } from "@/lib/validations";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 export default SignIn;
