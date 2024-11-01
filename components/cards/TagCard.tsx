c3510485624c105040b5e1292a602783570d4c3c
ataberkakkan <akkanataberk@gmail.com>
2024-11-01 15:47:54 +0300

    Tags Input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import ROUTES from "@/constants/routes";
@@ -11,6 +12,9 @@ interface TagCardProps {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
 const TagCard = ({
@@ -19,22 +23,48 @@ const TagCard = ({
   questions,
   showCount,
   compact,
+  remove,
+  isButton,
+  handleRemove,
 }: TagCardProps) => {
   const iconClass = getDeviconClassName(name);
 
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+  const Content = (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            width={12}
+            height={12}
+            alt="Close Icon"
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
 
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
       )}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button type="button" className="flex justify-between gap-2">
+        {Content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        {Content}
+      </Link>
+    );
+  }
 };
 export default TagCard;
