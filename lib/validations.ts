39f420d27f68140e7c4b193e17958ca97e1ff727
ataberkakkan <akkanataberk@gmail.com>
2024-11-18 13:14:37 +0300

    Create & Read User Routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -66,3 +66,19 @@ export const AskQuestionSchema = z.object({
     .min(1, { message: "At least 1 tag is required" })
     .max(3, { message: "Cannot add more than 3 tags" }),
 });
+
+export const UserSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters." }),
+  email: z.string().email({ message: "Please provida a valid email address." }),
+  bio: z.string().optional(),
+  image: z.string().url({ message: "Please provide a valid URL" }).optional(),
+  location: z.string().optional(),
+  portfolio: z
+    .string()
+    .url({ message: "Please provide a valid URL" })
+    .optional(),
+  reputation: z.number().optional(),
+});
