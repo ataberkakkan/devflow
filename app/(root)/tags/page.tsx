cd122cf77891aab5bf006bfaedca0fd761ec687e
ataberkakkan <akkanataberk@gmail.com>
2025-01-07 09:53:00 +0300

    getTags action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,4 +1,13 @@
-const Tags = () => {
+import { getTags } from "@/lib/actions/tag.action";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+  });
+
+  const { tags } = data || {};
+
   return <div>Tags</div>;
 };
 export default Tags;
