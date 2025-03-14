1a454fe21707453592c0998d17839835440f129b
ataberkakkan <akkanataberk@gmail.com>
2025-03-14 09:16:55 +0300

    Optimization

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -9,13 +9,10 @@ import TagCard from "../cards/TagCard";
 import DataRenderer from "../DataRenderer";
 
 const RightSidebar = async () => {
-  const { success, data: topQuestions, error } = await getTopQuestions();
-
-  const {
-    success: tagSuccess,
-    data: popularTags,
-    error: tagError,
-  } = await getTopTags();
+  const [
+    { success, data: topQuestions, error },
+    { success: tagSuccess, data: popularTags, error: tagError },
+  ] = await Promise.all([getTopQuestions(), getTopTags()]);
 
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
