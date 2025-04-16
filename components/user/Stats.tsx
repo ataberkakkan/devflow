890653e6d22334f6d94d401c6ddd34e28fe6b4e2
ataberkakkan <akkanataberk@gmail.com>
2025-04-16 14:04:01 +0300

    Display Reputation on Profile

--- components/user/Stats.tsx
+++ components/user/Stats.tsx
@@ -6,6 +6,7 @@ interface StatsProps {
   totalQuestions: number;
   totalAnswers: number;
   badges: BadgeCounts;
+  reputationPoints: number;
 }
 
 interface StatsCardProps {
@@ -28,10 +29,20 @@ const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
   );
 };
 
-const Stats = ({ totalQuestions, totalAnswers, badges }: StatsProps) => {
+const Stats = ({
+  totalQuestions,
+  totalAnswers,
+  badges,
+  reputationPoints,
+}: StatsProps) => {
   return (
     <div className="mt-3">
-      <h3 className="h3-semibold text-dark200_light900">Stats</h3>
+      <h3 className="h3-semibold text-dark200_light900">
+        Stats{" "}
+        <span className="small-semibold primary-text-gradient">
+          {formatNumber(reputationPoints)}
+        </span>
+      </h3>
 
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
