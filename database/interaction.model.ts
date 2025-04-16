687b51d535634cd84671afd4c0b53e5b20df7b08
ataberkakkan <akkanataberk@gmail.com>
2025-04-16 14:48:35 +0300

    Update Reputation Logic

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -4,15 +4,26 @@ export interface IInteraction {
   user: Types.ObjectId;
   action: string;
   actionId: Types.ObjectId;
-  actionType: "question" | "answer";
+  actionType: string;
 }
+
+export const InteractionActionEnums = [
+  "view",
+  "upvote",
+  "downvote",
+  "bookmark",
+  "post",
+  "edit",
+  "delete",
+  "search",
+] as const;
 
 export interface IInteractionDoc extends IInteraction, Document {}
 
 const InteractionSchema = new Schema<IInteraction>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    action: { type: String, required: true },
+    action: { type: String, enum: InteractionActionEnums, required: true },
     actionId: { type: Schema.Types.ObjectId, required: true },
     actionType: { type: String, enum: ["question", "answer"], required: true },
   },
